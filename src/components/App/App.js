d84c5f02ea9cd9d6780affac809f28571f50e9cf
rubensysoecho <rubenmartinezTRABALLOS@gmail.com>
2022-03-28 19:38:38 +0200

    started

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='hero'>
+        <header>
+          
+        </header>
+        <div className='hero__content'>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
